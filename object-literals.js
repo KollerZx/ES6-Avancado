fa075cfcd987be4f3144da7ee71af43cc46ff812
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2020-05-13 17:26:29 -0300

    Update object-literals.js

--- object-literals.js
+++ object-literals.js
@@ -18,3 +18,48 @@ var obj = {
 };
 console.log(obj);
 
+/*Aplicando o mesmo conceito em métodos*/
+function method1() {
+    console.log('method called');
+}
+var obj = {
+    method1
+};
+obj.method1();
+
+/*
+var obj = {
+    sum: function sum(a, b){
+        return a + b;
+    }
+};
+
+console.log(obj.sum(1, 5));*/
+
+/*Um outro short range do Ecma6 
+A mesma função de soma pode ser escrita omitindo a palavra function*/
+var obj = {
+    sum(a, b){
+        return a + b;
+    }
+};
+
+console.log(obj.sum(1, 5));
+
+/*Antigamente caso quisesse utilizar uma variavel como nome de uma propriedade, deveriamos declarar o objeto literal antes e acessa-lo
+através dos []
+
+var propName = 'test';
+var obj = {};
+
+obj[propName + 'concat'] = 'prop value';
+
+console.log(obj);*/
+
+/*Com o surgimento do Ecma6 é possivel fazer isso direto no objeto*/
+
+var propName = 'test';
+var obj = {
+    [propName + 'concat']: 'prop value'
+};
+console.log(obj);
