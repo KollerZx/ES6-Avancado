dd246b5eeb4f23ae090ce43693acb5fa0bab28f8
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2020-05-14 22:35:25 -0300

    Update rest-spreadOperator.js

--- rest-spreadOperator.js
+++ rest-spreadOperator.js
@@ -50,8 +50,37 @@ console.log(sum(5, 6, 4, 3, 2));
 
 /*Quebra a string em caracteres e joga para uma lista*/
 const stri = 'JavaScript ES6';
+const arr = [1, 2, 3, 4];
 function logArgs(...args) {
     console.log(args);
 }
 logArgs(...stri);
+logArgs(...arr);
 //(14) ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t", " ", "E", "S", "6"]
+//(4) [1, 2, 3, 4]
+
+//Concatenando arrays com spread operator
+const arr2 = [...arr, 5, 6, 7];
+console.log(arr2);
+//(7) [1, 2, 3, 4, 5, 6, 7]
+
+/*Quando se utiliza spread operator em objetos literais, só podem ser utilizados para construir novos objetos
+A ordem das chaves é importante, se sobrescrevermos uma propriedade que ja existe em outro objeto*/
+
+const obj = {
+    test: 123
+};
+const obj2 = {
+    ...obj,
+    test2: 'hello'
+};
+const obj3 = {
+    ...obj,
+    test: 456 //como test é uma propriedade existente em 'obj', irá sobrescrever e retornará apenas {test: 456}.
+};
+console.log(obj2);
+console.log(obj3);
+
+/*Não é possivel utilizar o spread em um objeto para construir um array*/
+const arr3 = [...obj];
+console.log(arr3); // Irá retornar um erro alertando que um objeto literal não é iteravel
