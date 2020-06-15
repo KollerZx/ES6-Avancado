c14c3d25d5beffb00563647bd4bdda0287ad3cab
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2020-06-15 13:22:22 -0300

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -11,12 +11,23 @@ console.log(apple, apple2);
 
 //Destructuring em Objetos
 var obj = {
-  name:'Henrique'
-}
+  name:'Henrique',
+  props:{
+    age: 26,
+    favoriteColors:['black','red' ]
+  }
+};
 
 //Para pegar a propriedade nome do obj e armazenar em uma variavel, antes do Ecma6 utilizava-se a seguinte sintaxe.
 var name = obj.name;
+var age = obj.props.age;
+var color = obj.props.favoriteColors[0];
 //Ja utilizando o Destructuring 
 
 var {name} = obj; //Ira destruir o obj {} encontrar uma propriedade 'name', pegar o seu valor e atribuir a uma variavel 'name'
 console.log(name);
+var {props:{age: age2, favoriteColors:[color1, color2]}} = obj;  //Destructuring de arrays dentro de objeto e vice versa (multiniveis)
+
+console.log(age2)
+
+//var {name: name2} = obj; //Ira destruir a propriedade name e atribuir a uma variavel name2 
